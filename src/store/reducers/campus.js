c04c25a7a432b570d36ccba74b9cc1949b36304c
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2021-05-12 15:33:50 -0400

    Add EDIT_CAMPUS, DELETE_CAMPUS cases in reducers

--- src/store/reducers/campus.js
+++ src/store/reducers/campus.js
@@ -1,4 +1,4 @@
-import { FETCH_CAMPUS } from "../actions/actionTypes";
+import { EDIT_CAMPUS, FETCH_CAMPUS } from '../actions/actionTypes';
 
 const initialState = {
   students: [],
@@ -8,6 +8,10 @@ const campus = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_CAMPUS:
       return action.payload;
+    case EDIT_CAMPUS:
+      return state.map((campus) => {
+        return campus.id === action.payload.id ? action.payload : campus;
+      });
     default:
       return state;
   }
--- src/store/reducers/campuses.js
+++ src/store/reducers/campuses.js
@@ -1,10 +1,19 @@
-import * as at from "../actions/actionTypes";
+import * as at from '../actions/actionTypes';
 
-// REDUCER;
-const allCampuses = (state = [], action) => {
+const initialState = {
+  allCampuses: [],
+};
+
+const allCampuses = (state = initialState, action) => {
   switch (action.type) {
     case at.FETCH_ALL_CAMPUSES:
       return action.payload;
+    case at.ADD_CAMPUS:
+      return [...state, action.payload];
+    case at.DELETE_CAMPUS:
+      return state.allCampuses.filter(
+        (campus) => campus.id !== action.payload.id
+      );
     default:
       return state;
   }
