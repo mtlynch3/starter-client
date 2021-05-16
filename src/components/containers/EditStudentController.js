48b69682cb30afd79ad07c1e09caa4f72044f8a6
Blake Vente <venteralph@gmail.com>
2021-05-16 05:11:21 -0400

    put requires validation, post, delete wip

--- src/components/containers/EditStudentController.js
+++ src/components/containers/EditStudentController.js
@@ -1,4 +1,4 @@
-import React, { Component, } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { editStudentThunk, fetchStudentThunk } from "../../store/thunks";
 import NavBarContainer from "../containers/NavBarContainer";
@@ -14,52 +14,102 @@ import {
 } from "reactstrap";
 
 class EditStudentContainer extends Component {
-  componentDidMount() {
-    //getting student ID from url
-    this.props.fetchStudent(this.props.match.params.id);
-
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    // this.state = {};
   }
-  handleSubmit(buttonEvent) {
+  async componentDidMount() {
+    //getting student ID from url
+    await this.props.fetchStudent(this.props.match.params.id);
+    this.setState(this.props.student);
+  }
+  handleChange(buttonEvent) {
     // prevent page refresh
     buttonEvent.preventDefault();
-    console.log(buttonEvent)
-
-    // clear the unmanaged items from the form
+    console.log(buttonEvent.target.name, buttonEvent.target.value);
+    this.setState({
+      ...this.state,
+      [buttonEvent.target.name]: buttonEvent.target.value,
+    });
   }
+  // updateState(buttonEvent)
 
   render() {
     return (
       <div>
         <NavBarContainer />
-        <Typography > Edit student</Typography>
-        <p>{JSON.stringify(this.props.student)}</p>
-        <Form onSubmit={this.handleSubmit} >
+        <pre>{JSON.stringify(this.state, null, 4)}</pre>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("Update");
+            this.props.updateStudent(this.state);
+            console.log(this.state);
+          }}
+        >
           <FormGroup>
-            <Label for="transaction">Transaction</Label>
-            <InputGroup size="lg">
+            <Label for="transaction">Edit Student</Label>
+            <InputGroup>
               <Input
-                placeholder="Description"
-                type="text"
+                type="number"
+                name="id"
+                placeholder={this.state?.id}
+                onChange={this.handleChange}
+                step=".01"
               />
               <Input
-                placeholder="Date"
-                type="date"
+                type="text"
+                name="firstName"
+                placeholder={this.state?.firstName}
+                onChange={this.handleChange}
+                step=".01"
+              />
+              <Input
+                type="text"
+                name="lastName"
+                placeholder={this.state?.lastName}
+                onChange={this.handleChange}
+                step=".01"
               />
             </InputGroup>
             <InputGroup>
-              <InputGroupAddon addonType="prepend">$</InputGroupAddon>
               <Input
-                placeholder="Amount"
                 min={0}
-                max={1000}
-                type="number"
-                step="1"
+                max={4}
+                type="email"
+                placeholder={this.state?.email}
+                name="email"
+                onChange={this.handleChange}
+                step=".01"
               />
-              <InputGroupAddon addonType="append">.00</InputGroupAddon>
+              <Input
+                min={0}
+                max={4}
+                type="number"
+                placeholder="GPA"
+                name="gpa"
+                onChange={this.handleChange}
+                step=".01"
+              />
             </InputGroup>
             <FormGroup />
-            <Button type="submit" value="Submit">
+            <Button type="submit" value="Submit" color="primary">
               Submit
+            </Button>
+            <Button
+              value="Delete"
+              color="danger"
+              onClick={() => alert("delte unimplemented")}
+            >
+              Delete
+            </Button>
+            <Button
+              value="create"
+              color="success"
+              onClick={() => alert(" unimplemented")}
+            >
+              Create
             </Button>
           </FormGroup>
         </Form>
