553a2e92f40b9651b173bcd433d9d98c71cb46c6
Muskan Kapoor <muskankapoor016@gmail.com>
2021-05-16 20:14:37 -0400

    parse information to display student view: update look for campus

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -3,19 +3,10 @@ import { useStyles } from "../../styles.js";
 const CampusView = ({campus}) => {
   const classes = useStyles();
 
-
   return (
     <div  className={classes.placeholderContent}>
       <h1>{campus.name}</h1>
       <p>{campus.description}</p>
-      <ul>
-        {/* {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <li key={student.id}>{name}</li>
-        );
-      })} */}
-      </ul>
     </div>
   );
 };
