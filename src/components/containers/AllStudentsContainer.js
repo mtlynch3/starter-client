9a7c043296e9fc8c8a93da59012acf5e9ea00466
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2021-05-22 16:57:15 -0400

    modified class

--- src/components/containers/AllStudentsContainer.js
+++ src/components/containers/AllStudentsContainer.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { fetchAllStudentsThunk } from "../../store/thunks";
-// import { AllStudentsView } from "../views";
+//import { AllStudentsView } from "../views";
 
 
 class AllStudentsContainer extends Component{
