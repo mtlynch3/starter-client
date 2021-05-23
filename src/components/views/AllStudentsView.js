e318cc63b3013d2a606e37b3ae53ee8811608d5c
winson65 <winson.yu22@gmail.com>
2021-05-23 17:07:43 -0400

    removed non-functioning form from AllStudentsView

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -8,29 +8,18 @@ const AllStudentsView = (props) => {
 
   return (
     <div>
-      <div>
-        <form className = "addStudentForm" onSubmit = {props.addStudent}>
-          <label htmlFor= "firstname">Enter First Name: </label>
-          <input type="text" id="firstname" name="firstname" />
-          <label htmlFor = "lastname"> Enter Last Name: </label>
-          <input type="text" id="lastname" name="lastname" />
-          <button type="submit" value= "Add Student">Add Student </button>
-        </form>
-      </div>
-      <div>
-        {props.allStudents.map( (student) => (
-          <div key = {student.id}>
-            <div> {student.firstname} </div>
-            <div> {student.lastname}  </div>
-            <div> GPA: {student.gpa} </div>
-            <img src = {student.imageUrl} alt="Student Portrait" width = "150" height = "150" />
-            <Link to={`/student/${student.id}`} >
-              <button type="button"> Show More Details</button>
-            </Link>
-            <br></br>
-          </div>
-        ))}
-      </div>
+      {props.allStudents.map( (student) => (
+        <div key = {student.id}>
+          <div> {student.firstname} </div>
+          <div> {student.lastname}  </div>
+          <div> GPA: {student.gpa} </div>
+          <img src = {student.imageUrl} alt="Student Portrait" width = "150" height = "150" />
+          <Link to={`/student/${student.id}`} >
+            <button type="button"> Show More Details</button>
+          </Link>
+          <br></br>
+        </div>
+      ))}
     </div>
   );
 };
