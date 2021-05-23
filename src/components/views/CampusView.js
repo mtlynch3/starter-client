f087f7be55c2da585fd00f6caf1cef4f960936b7
Stacey Li <listacey22@yahoo.com>
2021-05-22 23:20:01 -0400

    styling

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,4 +1,6 @@
-
+import './style/Campus.css'
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 const CampusView = (props) => {
   const {campus} = props;
@@ -7,17 +9,36 @@ const CampusView = (props) => {
   //   return <div>Loading...</div>
   // }
   return (
-    <div>      
-      <h1>{campus.name}</h1>
-      <p>{campus.description}</p>
-      <ul>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <li key={student.id}>{name}</li>
-        );
-      })}
-      </ul>
+    <div>
+      <div id = "details">
+        <img class = "school" src={campus.imageUrl} alt="picture of college"/>
+        <div id = "text">
+          <h1>{campus.name}</h1>
+          <p>{campus.address}</p>
+          <p>{campus.description}</p>
+          // <Link to={'/campuses'} >
+            <Button variant="contained" color="primary">
+              Edit
+            </Button>
+          // </Link>
+          // <Link to={'/campuses'} >
+            <Button variant="contained" color="primary">
+              Delete
+            </Button>
+          // </Link>
+        </div>
+      </div>
+      <div id = "student">
+        <h1> Students on Campus </h1>
+        <ul>
+        {campus.students.map( student => {
+          let name = student.firstname + " " + student.lastname;
+          return (
+            <li key={student.id}>{name}</li>
+          );
+        })}
+        </ul>
+      </div>
     </div>
   );
 
