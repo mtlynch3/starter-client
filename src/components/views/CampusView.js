beca8b314ce89cd4d377d6b8f7d0e64edb49f84e
Shafali Gupta <shafaligupta31@gmail.com>
2021-12-10 15:58:34 -0500

    added links to student to single campus view

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,9 +1,65 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+    fontType: 'bold',
+    fontFamily: 'Courier, sans-serif', 
+    fontSize: '35px', 
+    color: '#CDDC39'
+  },
+  appBar:{
+    backgroundColor: '#11153e',
+    shadows: ['none'],
+  },
+  greeting:{
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    width: "50%",
+    margin: "auto",
+  },
+  links:{
+    textDecoration: 'none',
+  }
+
+}));
 
 const CampusView = (props) => {
+  const classes = useStyles();
+
   const {campus} = props;
   return (
-    <div>      
+    <div className={classes.root}>
+      <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit" >
+            CRUD App
+          </Typography>
+
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              All Campuses
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
+              All Students
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>     
       <h1>{campus.name}</h1>
       <img src={campus.imageUrl}/>
       <p> {campus.address} </p>
@@ -16,7 +72,10 @@ const CampusView = (props) => {
             {campus.students.map( student => {
               let name = student.firstname + " " + student.lastname;
               return (
-                <li key={student.id}>{name}</li>
+                // <li key={student.id}>{name}</li>
+                <li key= {student.id}> <Link to={`/student/${student.id}`}>
+                <p>{name}</p> 
+              </Link> </li>
               );
             })}
             </ul>
